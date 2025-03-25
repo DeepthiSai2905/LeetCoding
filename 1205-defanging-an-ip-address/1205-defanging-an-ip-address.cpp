aff0fcd6f4a187cd2077ffb600b4d5ddd549fbca
Deepthi <49442816+Deepthi2001@users.noreply.github.com>
2025-03-25 17:57:00 -0400

    Time: 2 ms (51.87%), Space: 7.5 MB (94.31%) - LeetHub

---
+++ 1205-defanging-an-ip-address/1205-defanging-an-ip-address.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        string res="";
+        for(char ch: address){
+            if(ch=='.'){
+                res+="[.]";
+            }
+            else res+=ch;
+        }
+        return res;
+    }
+};
