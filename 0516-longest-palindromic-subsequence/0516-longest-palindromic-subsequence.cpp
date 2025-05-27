98e121e0c7cb1261d8b7c2635ac886416fc1a184
Deepthi <49442816+Deepthi2001@users.noreply.github.com>
2025-05-27 14:21:33 -0400

    Time: 51 ms (79.85%), Space: 9.5 MB (91.7%) - LeetHub

---
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int m=text1.length();
+        int n=text2.length();
+        vector<int>prev(n+1,0);
+        vector<int>curr(n+1,0);
+        prev[0]=0; curr[0]=0;
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(text1[i-1]==text2[j-1]){
+                    curr[j]=1+prev[j-1];
+                }
+                else{
+                    curr[j]=max(prev[j],curr[j-1]);
+                }
+            }
+            prev=curr;
+        }
+        return prev[n];
+    }
+    int longestPalindromeSubseq(string s) {
+        string rev = s;
+        reverse(rev.begin(),rev.end());
+        return longestCommonSubsequence(s,rev);
+    }
+};
