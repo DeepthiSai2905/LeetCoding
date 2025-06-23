9201249c6b4ee099947104cad4475a09a367a7ec
Deepthi <49442816+Deepthi2001@users.noreply.github.com>
2025-06-23 17:28:18 -0400

    Time: 0 ms (100%), Space: 10 MB (43.55%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
-    void subsetsHelper(vector<int>& nums, vector<vector<int>>&res, int pivot, vector<int>&path){
-        // if(pivot==nums.size()) {
-        //     return ;
-        // } i wont be able to add leaves (1,2,3),(2,3),(1,3)
-        res.push_back(path);
-        for(int i=pivot;i<nums.size();i++){
-            path.push_back(nums[i]);
-            subsetsHelper(nums,res,i+1,path);
-            path.pop_back();
-        }
-    }
     vector<vector<int>> subsets(vector<int>& nums) {
+        // no recursion
+        int n=nums.size();
         vector<vector<int>>res;
         vector<int>path;
-        subsetsHelper(nums,res,0,path);
+        res.push_back({});
+        for(int i=0;i<n;i++){
+            int resSize = res.size();
+            for(int j=0;j<resSize;j++){
+                vector<int>path=res[j];
+                path.push_back(nums[i]);
+                res.push_back(path);
+            }
+        }
         return res;
     }
 };
