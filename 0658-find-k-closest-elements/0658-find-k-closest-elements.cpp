3e4ceba9cd83971e68dc94a08ebae1b7b827de4b
Deepthi <49442816+Deepthi2001@users.noreply.github.com>
2025-06-26 00:13:09 -0400

    Time: 4 ms (50.09%), Space: 36.4 MB (54.14%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
-    struct comp{
-        bool operator()(const pair<int,int>&a, const pair<int,int>&b){
-            if(a.first==b.first){ // distance same
-               return a.second<b.second; // a has low priority, b on top - max heap
-            }
-            return a.first<b.first;
-        }
-    };
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        vector<int> result;
-        // k closest - maxheap
-        priority_queue<pair<int,int>, vector<pair<int,int>>, comp>pq; // dist,value
-        for(int num:arr){ // nlogk
-            pq.push({abs(x-num),num});
-            if(pq.size()>k) pq.pop();
+        int l=0,r=arr.size()-1;
+        while(r-l+1>k){
+            int distL = abs(x-arr[l]);
+            int distR = abs(x-arr[r]);
+            if(distL<distR) r--;
+            else if(distL>distR) l++;
+            else { // move pointer which has greater value 
+              if(arr[l]>arr[r]) l++;
+              else r--;
+            }
         }
-        while(!pq.empty()){
-            result.push_back(pq.top().second);
-            pq.pop();
+        vector<int>result;
+        for(int i=l;i<=r;i++){
+            result.push_back(arr[i]);
         }
-        sort(result.begin(),result.end()); // klogk
+        // time - o(n), space-o(1)
         return result;
 
     }
