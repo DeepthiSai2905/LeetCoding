699e0d23b4d15e9886d039261429bca301232103
Deepthi <49442816+Deepthi2001@users.noreply.github.com>
2025-06-27 20:51:09 -0400

    Time: 4 ms (15.84%), Space: 14.1 MB (97.93%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -1,17 +1,35 @@
 class Solution {
-public:
+public: // solution for sorted arrays
+    int firstOccurence(vector<int>& nums, int target, int i,int j){
+        int res = -1;
+        while(i<=j){
+            int mid=i+(j-i)/2;
+            if(nums[mid]==target){
+                res=mid;
+                j=mid-1;
+            }
+            else if(nums[mid]<target){
+                i=mid+1;
+            }
+            else j=mid-1;
+        }
+        return res;
+    }
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
         int m=nums1.size(); // considering m small
         int n=nums2.size();
         if(m>n) return intersect(nums2,nums1);
+
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+        // mlogn - search each element of smaller array in bigger array using binary search
         vector<int>result;
-        unordered_map<int,int>mp;
-        for(int num:nums1) mp[num]++;
-        for(int ele:nums2){
-            if(mp[ele]>0){
-                result.push_back(ele);
-                mp[ele]--;
-            }
+        int low=0,high=n-1;
+        for(int num:nums1){
+            int bsIndx = firstOccurence(nums2,num, low,high);
+            if(bsIndx==-1) continue;
+            low=bsIndx+1;
+            result.push_back(num);
         }
         return result;
     }
