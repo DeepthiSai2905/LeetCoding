a7b47d6917ef4bd0715f1051929daa6f46d8a809
Deepthi <49442816+Deepthi2001@users.noreply.github.com>
2025-06-29 11:35:06 -0400

    Time: 0 ms (100%), Space: 12.3 MB (91.84%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -9,7 +9,7 @@ public:
         while(i<=j){
             int mid=i+(j-i)/2;
             if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]) return mid;
-            else if(nums[mid]>nums[mid-1]) i=mid+1;
+            else if(nums[mid]<nums[mid+1]) i=mid+1;
             else j=mid-1;
         }
         return -1;
