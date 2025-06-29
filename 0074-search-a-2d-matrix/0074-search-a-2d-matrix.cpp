e7abb4fa6b9a2434e6e525f83a9e9d5da7c3c4d2
Deepthi <49442816+Deepthi2001@users.noreply.github.com>
2025-06-28 22:07:55 -0400

    Time: 0 ms (100%), Space: 13.6 MB (7.21%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,22 +1,45 @@
 class Solution {
 public:
+    int binarySearch(vector<int>arr,int target){
+        int low=0, high=arr.size()-1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         if(matrix.size()==0) return false;
         int m=matrix.size();
         int n=matrix[0].size();
-        int i=0, j=m*n-1;
-        while(i<=j){
-            int mid=i+(j-i)/2;
-            int midR=(mid/n), midC=(mid%n);
-            if(matrix[midR][midC]==target){
-                return true;
-            }
-            else if(matrix[midR][midC]<target){
-                i=mid+1;
-            }
-            else j=mid-1;
+        // traverse last column to find which row can have target 
+        // apply binary search to last column to find target or immediate value which >=target (lower bound)
 
+        int res=-1;
+        int low=0, high=m-1;
+        int targetRow=-1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (matrix[mid][n-1] == target) {
+                targetRow=mid; break;
+            } else if (matrix[mid][n-1] < target) {
+                low = mid + 1;
+            } else {
+                targetRow=mid;
+                high = mid - 1;
+            }
         }
-        return false;
+        if (targetRow == -1) return false;
+        // binary search on targetRow to check for target
+        int targetCol = binarySearch(matrix[targetRow],target);
+        if(targetCol==-1) return false;
+        return true;
     }
 };
