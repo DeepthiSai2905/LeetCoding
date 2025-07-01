83eb8a57e27a3002d0908799d4bd76c06e2b8732
Deepthi <49442816+Deepthi2001@users.noreply.github.com>
2025-07-01 18:09:22 -0400

    Time: 78 ms (7.05%), Space: 37.2 MB (6.04%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
-    string decodeString(string s){
-        vector<int>v(26,0);
-        for(char c:s){
-            v[c-'a']++;
-        }
-        string res;
-        for(char c='a';c<='z';c++){
-           if(v[c-'a']!=0){
-            res+=c;
-            res+=to_string(v[c-'a']);
-           } 
-        }
-        return res;
-    }
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         vector<vector<string>>res;
         unordered_map<string, vector<string>>mp;
-        for(string s:strs){
-            string decode=decodeString(s);
-            mp[decode].push_back(s);
+        for(string word:strs){
+            vector<int> count(26, 0);
+
+            for (char c : word) {
+                count[c - 'a']++;
+            }
+
+            string decode;
+            for (int i = 0; i < 26; ++i) {
+                decode += '#' + to_string(count[i]); // Use '#' as delimiter
+            }
+            mp[decode].push_back(word);
         }
         for(auto x:mp){
             res.push_back(x.second);
