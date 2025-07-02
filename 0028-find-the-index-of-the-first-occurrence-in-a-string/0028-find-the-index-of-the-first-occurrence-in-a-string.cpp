f856eeb71e71229c13ce86445e994be32f4377cc
Deepthi <49442816+Deepthi2001@users.noreply.github.com>
2025-07-02 19:42:33 -0400

    Time: 0 ms (100%), Space: 8.6 MB (58.59%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -3,21 +3,33 @@ public:
     int strStr(string haystack, string needle) {
         int m=needle.length();
         int n=haystack.length();
-        int i=0,j=0;
-        while(i<=n-m){
-            if(haystack[i]==needle[0]){ // if found first char
-               int k=i,j=0;
-               while(haystack[k]==needle[j]){
-                k++; j++;
-                if(j==m) return i; // match found
-               }
-               // if match not found
-               i++;
-            }
-            else{
-                i++;
-            }
+        if(m>n) return -1;
+        const int mod = 1e9 + 7;
+        long hashP=0,k=26;
+        for(char ch:needle){
+            hashP = (hashP*k+(ch-'a'+1))%mod;
         }
+        
+        long hashS=0;
+        // long long power = pow(k, m-1);
+        long long power = 1;
+        for (int i = 1; i < m; i++) {
+            power = (power * k) % mod;
+        }
+        for(int i=0;i<n;i++){
+            long out=0;
+            if(i>=m){ // actual sliding window
+               // outgoing char
+               char outChar = haystack[i-m];
+                out = ((outChar-'a'+1)*power)%mod;
+                hashS= (hashS-out+mod)%mod;
+            }
+            hashS = (hashS*k+(haystack[i]-'a'+1))%mod; // add current char
+            
+            if(i >= m - 1 && hashS==hashP) return i-m+1;
+        }
+
+        
         return -1;
     }
 };
