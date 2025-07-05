8208dce832ea96c1959fb067d85a147135a0efd8
Deepthi <49442816+Deepthi2001@users.noreply.github.com>
2025-07-05 08:23:49 -0400

    Time: 17 ms (20.77%), Space: 19.1 MB (65.11%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.cpp
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.cpp
@@ -18,22 +18,26 @@ public:
 
 class Solution {
 public:
-    void dfsConnect(Node* root){
+    void dfsConnect(Node* left,Node* right){
         // preorder
-        if(root->left==NULL) return ; // for level
+        // if(root->left==NULL) return ; // for level this becomes
+        if(left==NULL) return;
         // logic
         // 1. connect leftchild to rightchild
-        root->left->next=root->right;
+        // root->left->next=root->right; This becomes
+        left->next=right;
         // 2. connect right of left node chunk to left of right node chunk 
-        if(root->next){
-            root->right->next = root->next->left;
-        }
-        dfsConnect(root->left);
-        dfsConnect(root->right);
+        // if(root->next){
+        //     root->right->next = root->next->left;
+        // } - this becomes
+        // right->next = next->left; - which covers under dfsConnect(left->right,right->left);
+        dfsConnect(left->left,left->right);
+        dfsConnect(left->right,right->left);
+        dfsConnect(right->left,right->right);
     }
     Node* connect(Node* root) {
         if(!root) return root;
-        dfsConnect(root);
+        dfsConnect(root->left,root->right);
         return root;
     }
 };
