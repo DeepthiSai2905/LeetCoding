2fd822becfae75de0498a0136411a43ca5400bd0
Deepthi <49442816+Deepthi2001@users.noreply.github.com>
2025-07-10 08:24:49 -0400

    Time: 3 ms (91.9%), Space: 15.3 MB (8.54%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -17,23 +17,32 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
+//  can we create random pointers while creating next pointers? in first stage itself
         if(!head) return head;
         unordered_map<Node*,Node*>mp;
         Node* curr=head;
-        // create clone of next pointers
+        // create clone of next pointers and random pointers
         while(curr){
-            // Node* copynode = 
-            mp[curr]=new Node(curr->val); // deep copy
-            // copycurr->next = copynode; // linking cloned nodes
-            curr=curr->next;
-            // copycurr = copycurr->next;
-        }
-        // handle random pointers now
-        curr=head;
-        while(curr){
-            mp[curr]->next = mp[curr->next];
-            mp[curr]->random = mp[curr->random];
-            // mp[curr]->random= curr->random ? mp[curr->random] : NULL;
+             // deep copy of next node
+            if (mp.find(curr) == mp.end()) {
+                mp[curr] = new Node(curr->val);
+            }
+            // make next pointer links
+            if (curr->next) {
+                if (mp.find(curr->next) == mp.end()) {
+                    // Clone the next node if not cloned yet
+                    mp[curr->next] = new Node(curr->next->val);  
+                }
+                mp[curr]->next = mp[curr->next];  // Link next pointer
+            }
+            // create deep copy of random node if not present else make link
+            if(curr->random){
+                if(mp.find(curr->random)==mp.end()){ // not present
+                   mp[curr->random] = new Node(curr->random->val);
+                  
+                }
+                mp[curr]->random = mp[curr->random]; //just link
+            }
             curr=curr->next;
         }
         return mp[head];
