58d5ddd98ab14195906616c1ccbebf4646a674c5
Deepthi <49442816+Deepthi2001@users.noreply.github.com>
2025-07-16 19:22:22 -0400

    Time: 0 ms (100%), Space: 26 MB (79.5%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        // brute -> trap possible - left taller right taller
-        int totalArea=0,currArea;
+        // optimal -> trap possible - left taller right taller
+        int totalArea=0;
         int n=height.size();
-        vector<int>leftMax(n);
-        vector<int>rightMax(n);
-        leftMax[0]=height[0];
-        for(int i=1;i<n;i++){
-            leftMax[i]=max(leftMax[i-1],height[i]);
-        }
-        rightMax[n-1]=height[n-1];
-        for(int i=n-2;i>=0;i--){
-            rightMax[i]=max(rightMax[i+1],height[i]);
-        }
-        for(int i=0;i<n;i++){
-            currArea=min(leftMax[i], rightMax[i])-height[i];
-            totalArea+=currArea;
+        int i=0,j=n-1;
+        int rightMax,leftMax;
+        while(i<=j){// always stop/endup at tallest building
+            if(height[i]<=height[j]){ // right more - check leftmax
+                if(leftMax>height[i]) {
+                    totalArea += leftMax-height[i];
+                }else{
+                    leftMax = height[i];
+                }
+                i++;
+            }else{
+                if(rightMax>height[j]){
+                    totalArea += rightMax-height[j];
+                }else{
+                    rightMax = height[j];
+                }
+                j--;
+            }
         }
         return totalArea;
     }
