15461e7b15a7650ace6eef2bcd8b0da4a3dc1891
Deepthi <49442816+Deepthi2001@users.noreply.github.com>
2025-07-16 20:00:47 -0400

    Time: 207 ms (5.24%), Space: 94.5 MB (5.5%) - LeetHub

--- 0561-array-partition/0561-array-partition.cpp
+++ 0561-array-partition/0561-array-partition.cpp
@@ -3,10 +3,29 @@ public:
     int arrayPairSum(vector<int>& nums) {
         int n=nums.size();
         int sum=0;
-        // sort and then take sum of 1st elements in pairs(min of 2 numbers)
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<n;i+=2){
-            sum+=nums[i];
+        int mini=INT_MAX,maxi=INT_MIN;
+        unordered_map<int,int>mp;
+        for(int i=0;i<n;i++){
+            if(nums[i]>maxi) maxi=nums[i];
+            if(nums[i]<mini) mini=nums[i];
+            mp[nums[i]]++;
+        }
+        int skipFlag=false; // if current element is computed, skip next
+        // window from mini to maxi
+        for(int num=mini;num<=maxi;num++){
+            int currCount=mp[num];
+            if(currCount<1) continue;
+            while(currCount>0){
+                if(skipFlag){ // skip
+                    currCount--;
+                    skipFlag=false;
+                }else{ // dont skip 
+                    sum+=num;
+                    currCount--;
+                    skipFlag=true;
+                }
+                mp[num]=0;
+            }
         }
         return sum;
     }
