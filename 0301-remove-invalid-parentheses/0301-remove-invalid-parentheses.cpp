8079d2c5765e40f71940f324f343f0ff023731e7
Deepthi <49442816+Deepthi2001@users.noreply.github.com>
2025-07-26 10:08:44 -0400

    Time: 211 ms (24.17%), Space: 33.5 MB (26.4%) - LeetHub

--- 0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.cpp
+++ 0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.cpp
@@ -1,5 +1,8 @@
 class Solution {
 public:
+    bool foundValid=false;
+     int maxLen = 0;
+
     bool isValid(string s){
         int count=0;
         for(char ch:s){
@@ -9,6 +12,45 @@ public:
         }
         return count==0;
     }
+    void dfs(string s, unordered_set<string>&vis,unordered_set<string>&result){
+        //base
+        if (vis.count(s)) return;
+
+        // logic 
+        vis.insert(s);
+        if (isValid(s)) {
+            int len = s.length();
+            if (len > maxLen) {
+                result.clear();
+                result.insert(s);
+                maxLen = len;
+            } else if (len == maxLen) {
+                result.insert(s);
+            }
+            return; // no need to remove more if valid
+        }
+        // now apply for loop recursion for each substring if not valid
+        else {
+            for(int j=0;j<s.length();j++){
+                if(s[j]>='a' && s[j]<='z') continue;
+                string child = s.substr(0,j)+s.substr(j+1);
+                // process only unvisited nodes
+                if(vis.count(child)==0){
+                    dfs(child,vis,result);
+                }
+            }
+        }
+        
+    }
+    vector<string> removeInvalidParentheses(string s) {
+        if(s.length()==0) return {};
+        unordered_set<string>result;
+        unordered_set<string>vis;
+        dfs(s,vis,result);
+        return vector<string>(result.begin(), result.end());
+    }
+    /* BFS
+    
     vector<string> removeInvalidParentheses(string s) {
         vector<string>result;
         unordered_set<string>vis;
@@ -46,5 +88,5 @@ public:
         return result;
 
         
-    }
+    } */
 };
