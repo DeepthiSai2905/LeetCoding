df3ba787028185ea5e0c67228e8d21c1750f9d14
Deepthi <49442816+Deepthi2001@users.noreply.github.com>
2025-07-26 13:58:52 -0400

    Time: 0 ms (100%), Space: 52.3 MB (33.34%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -2,22 +2,11 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
         int n=nums.size();
-        if(n<2) return true;
-        queue<int>Q;
-        unordered_set<int>s; // to aviod repeated computatinos in recursion
-        Q.push(0);
-        s.insert(0);
-        while(!Q.empty()){
-            int curr=Q.front(); Q.pop();
-            for(int j=1;j<=nums[curr];j++){
-                int newInd = curr+j;
-                if(newInd==n-1) return true;
-                if(!s.count(newInd)){
-                    Q.push(newInd);
-                    s.insert(newInd);
-                }
-            }
+        int canMaxReach=0;
+        for(int i=0;i<n;i++){
+            if(i>canMaxReach) return false;
+            canMaxReach = max(canMaxReach,i+nums[i]);
         }
-        return false;
+        return true;
     }
 };
