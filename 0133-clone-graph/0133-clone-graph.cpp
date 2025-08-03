1f9c39ddf44353c8f24ca19538235b64c9a92de2
Deepthi <49442816+Deepthi2001@users.noreply.github.com>
2025-08-03 15:43:56 -0400

    Time: 7 ms (22.01%), Space: 12.3 MB (30.91%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.cpp
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -21,26 +21,23 @@ public:
 
 class Solution {
 public:
+    void dfs(Node* curr, unordered_map<Node*, Node*>&mp){
+        // base
+        if(mp.find(curr)!=mp.end()) return ; // node present
+        // logic
+        Node* clonedNode=new Node(curr->val);
+        mp[curr]=clonedNode;
+        for(Node* neigh: curr->neighbors){
+            // then only create cloned nodes
+            dfs(neigh, mp);
+            // we have all neigh cloned nodes, create connections now
+            mp[curr]->neighbors.push_back(mp[neigh]);
+        }
+    }
     Node* cloneGraph(Node* node) {
         if(node==NULL) return NULL;
         unordered_map<Node*, Node*>mp;
-        queue<Node*>Q;
-        Node* clonedNode=new Node(node->val);
-        mp[node]=clonedNode;
-        Q.push(node); // iterate original graph
-        while(!Q.empty()){
-            Node* curr = Q.front(); Q.pop();
-            for(Node* neigh: curr->neighbors){
-                if(mp.find(neigh)==mp.end()){ // not found
-                   // then only create cloned nodes
-                   Node* clonedNeigh=new Node(neigh->val);
-                   Q.push(neigh);
-                   mp[neigh]=clonedNeigh;
-                }
-                // we have all neigh cloned nodes, create connections now
-                mp[curr]->neighbors.push_back(mp[neigh]);
-            }
-        }
-        return clonedNode;
+        dfs(node,mp);
+        return mp[node];
     }
 };
