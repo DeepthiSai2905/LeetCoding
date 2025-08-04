0ac4982923ccb02497af81da4fbcb8dadd2354c4
Deepthi <49442816+Deepthi2001@users.noreply.github.com>
2025-08-04 09:13:45 -0400

    Time: 0 ms (100%), Space: 8.2 MB (81.23%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -3,24 +3,29 @@ public:
     bool isMatch(string s, string p) {
         int m=s.length();
         int n=p.length();
-        vector<vector<bool>>dp(m+1,vector<bool>(n+1,false));
-        dp[0][0]=true; //empty string
+        vector<bool>dp(n+1);
+        dp[0]=true; //empty string
         for(int j=1;j<=n;j++){
             if(p[j-1]=='*'){
                 // go 2 steps back
-                dp[0][j]=dp[0][j-2];
+                dp[j]=dp[j-2];
             }
         }
-        for(int i=1;i<=m;i++){
+        for(int i=1;i<=m;i++){ 
+            bool diagUp=dp[0];
+            dp[0]=false;
             for(int j=1;j<=n;j++){
+                bool temp=dp[j];
                 if(p[j-1]=='*'){
                     // 0 1
                     // 0 1 case - curr char at src == prev char of * in p
                     if(s[i-1]==p[j-2] || p[j-2]=='.'){ 
-                        dp[i][j]=dp[i-1][j] || dp[i][j-2];
+                        //dp[i][j]=dp[i-1][j] || dp[i][j-2];
+                        dp[j]=dp[j] || dp[j-2];
                     }
                     // 0 caseis always available and will 2 steps back
-                    else  dp[i][j]=dp[i][j-2]; // only 0 case
+                    // else  dp[i][j]=dp[i][j-2]; // only 0 case
+                    else dp[j]=dp[j-2];
                 }
                 else{ // character
                    // check if they match
@@ -28,13 +33,16 @@ public:
                     /* if . no need to match as it can 
                     match with any character(considered matched) */
                     // matched - left diag
-                      dp[i][j]=dp[i-1][j-1];
+                      dp[j]=diagUp;
+                   }else{
+                    dp[j]=false;
                    }
-
                 }
+
+                diagUp=temp;
             }
         }
-        return dp[m][n];
+        return dp[n];
         
     }
 };
