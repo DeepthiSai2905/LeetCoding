5815fab15d2f33d763a0bdaf2e9c67ea8e802eb9
Deepthi <49442816+Deepthi2001@users.noreply.github.com>
2025-08-03 21:11:35 -0400

    Time: 0 ms (100%), Space: 9.2 MB (41.63%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -3,7 +3,7 @@ public:
     bool isMatch(string s, string p) {
         int m=s.length();
         int n=p.length();
-        vector<vector<bool>>dp(m+1,vector<bool>(n+1,0));
+        vector<vector<bool>>dp(m+1,vector<bool>(n+1,false));
         dp[0][0]=true; //empty string
         for(int j=1;j<=n;j++){
             if(p[j-1]=='*'){
@@ -29,9 +29,6 @@ public:
                     match with any character(considered matched) */
                     // matched - left diag
                       dp[i][j]=dp[i-1][j-1];
-                   }
-                   else{
-                      dp[i][j]=false;
                    }
 
                 }
