c51503f281a10a55ff8cc8e453dd532aec9511d0
Deepthi <49442816+Deepthi2001@users.noreply.github.com>
2025-08-05 14:15:29 -0400

    Time: 0 ms (100%), Space: 9.8 MB (26.75%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
@@ -1,40 +1,36 @@
 class MyQueue {
 public:
-  // pop heavy
+  // push heavy, pop top light
     stack<int>s1;
     stack<int>s2;
     MyQueue() {
     }
     
     void push(int x) {
-        s1.push(x);
-    }
-    
-    int pop() {
-       /* 
+        /* 
        1. shift elements of s1 to s2 
-       2. pop top of s2
-       3.  shift elements of s2 to s1 back to keep push in o(1)
+       2. push element to s1
+       3. shift elements of s2 to s1 back to keep pop in o(1)
        */
        while(!s1.empty()){
         s2.push(s1.top()); s1.pop();
        }
-       int poppedEle = s2.top(); s2.pop();
+       s1.push(x);
        while(!s2.empty()){
         s1.push(s2.top()); s2.pop();
        }
+       
+    }
+    
+    int pop() {
+       if(s1.empty()) return -1;
+       int poppedEle = s1.top(); s1.pop();
        return poppedEle;
     }
     
     int peek() {
-        while(!s1.empty()){
-        s2.push(s1.top()); s1.pop();
-       }
-       int topEle = s2.top();
-       while(!s2.empty()){
-        s1.push(s2.top()); s2.pop();
-       }
-       return topEle;
+       if(s1.empty()) return -1;
+       return s1.top();
     }
     
     bool empty() {
