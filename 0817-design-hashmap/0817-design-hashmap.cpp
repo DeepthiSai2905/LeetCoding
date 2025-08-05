2cd2a3a0c326f50417d23084fe186f831b99499c
Deepthi <49442816+Deepthi2001@users.noreply.github.com>
2025-08-04 21:12:04 -0400

    Time: 16 ms (83.46%), Space: 63.6 MB (57.35%) - LeetHub

---
+++ 0817-design-hashmap/0817-design-hashmap.cpp
@@ -0,0 +1,84 @@
+class MyHashMap {
+public:
+    class Node{
+    public:
+        int key;
+        int value;
+        Node* next;
+        Node(int key, int value) {
+            this->key = key;
+            this->value = value;
+            this->next = NULL;
+        }
+    };
+    // private: vector<Node*> storage;
+    // private: int buckets;
+    // private: int hashFunction(int key){
+    //     return key%buckets;
+    // }
+
+    vector<Node*> storage;
+    int buckets;
+    int hashFunction(int key){
+        return key%buckets;
+    }
+
+    MyHashMap() {
+        buckets=10000;
+        storage = vector<Node*>(buckets);
+    }
+    
+    Node* helper(Node* head, int key){
+        Node* prev=NULL;
+        Node* curr=head;
+        while(curr && curr->key!=key){
+            prev=curr;
+            curr=curr->next;
+        }
+        return prev;
+    }
+    void put(int key, int value) {
+        int hashIndex = hashFunction(key);
+        Node* head = storage[hashIndex];
+        if(head==NULL) {
+            head=new Node(-1,-1);
+        }
+        storage[hashIndex]=head;
+        Node* prev = helper(head, key);
+        // present in map or not
+        if(prev->next && prev->next->key == key){ // key present - not fresh
+           prev->next->value=value; // replace existing value
+        }else{ // fresh node
+            prev->next = new Node(key,value);
+        }
+    }
+    
+    int get(int key) {
+        int hashIndex = hashFunction(key);
+        Node* head = storage[hashIndex];
+        if(!head) return -1;
+        Node* prev = helper(head, key);
+        if(!prev->next) return -1;
+        return prev->next->value;
+    }
+    
+    void remove(int key) {
+        int hashIndex = hashFunction(key);
+        Node* head = storage[hashIndex];
+        if(!head) return ;
+        Node* prev = helper(head, key);
+        if(!prev->next) return ;
+        Node* temp = prev->next;
+        prev->next = prev->next->next;
+        delete temp;
+
+    }
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
