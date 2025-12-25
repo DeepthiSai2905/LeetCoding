2c5e24b71b172111486030642f2dbbf73fd09147
Deepthi <49442816+Deepthi2001@users.noreply.github.com>
2025-12-25 11:23:51 -0500

    Time: 0 ms (100%), Space: 9.5 MB (45.61%) - LeetHub

--- 0541-reverse-string-ii/0541-reverse-string-ii.cpp
+++ 0541-reverse-string-ii/0541-reverse-string-ii.cpp
@@ -4,23 +4,17 @@ public:
         int n=s.length();
         int i=0;
         string result;
-        while(i<n){
-            if(i+2*k > n){ // last set
-               int remStrlen = n-i; // leftover string
-               if(remStrlen<k){
-                 reverse(s.begin()+i, s.end()); // reverse all
-               }
-               else{
-                reverse(s.begin()+i, s.begin()+i+k); // reverse first k
-               }
-                result+=s.substr(i);
-                break;
-            }
-            else{
-                // take 2k characters, reverse first k
-                reverse(s.begin()+i,s.begin()+i+k);
-                i+=2*k;
-            }
+        while(i+2*k<n){
+            reverse(s.begin()+i,s.begin()+i+k);
+            i+=2*k;
+        }
+        int remStrlen = n-i; // leftover string
+        
+        if(remStrlen<k){
+            reverse(s.begin()+i, s.end()); // reverse all
+        }
+        else{
+        reverse(s.begin()+i, s.begin()+i+k); // reverse first k
         }
         return s;
     }
